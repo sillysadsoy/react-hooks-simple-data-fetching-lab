0b7c12d77922f3a449561096161660c17eef7957
roger chat <mattrogerpelosi@gmail.com>
2021-09-05 14:42:52 -0400

    commit tree f26ac8149e35d6bb75141388733a69c04b3e15d8

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import { useState, useEffect } from 'react';
+
+function App() {
+    const [imgSrc, setImgSrc] = useState('');
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => setImgSrc(data.message));
+    }, []);
+
+    return (
+        <div>
+            <p>Loading...</p>
+            <img alt='A Random Dog'
+                 src={imgSrc} />
+         </div>
+    );
+};
+
+export default App;
